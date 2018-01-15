6b7550e30962a9343e5d980c40878974b25f37fb
Unknown <zulachmod755@gmail.com>
2018-01-16 03:50:01 +0800

    commit-latest

--- TugasAPI(MainAPI)/app/src/main/java/net/zhulachmad/tugasapi/MainActivity.java
+++ TugasAPI(MainAPI)/app/src/main/java/net/zhulachmad/tugasapi/MainActivity.java
@@ -1,18 +1,29 @@
 package net.zhulachmad.tugasapi;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import java.io.IOException;
 import android.os.AsyncTask;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,14 +37,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        EditText nophone = (EditText) findViewById(R.id.et_phone);
+        EditText pesan = (EditText) findViewById(R.id.et_pesan);
+
+
         client = new OkHttpClient();
         try {
-            postdata.put("msisdn", "082296295744");
-            postdata.put("content", "testing");
+            postdata.put("msisdn", nophone.getText().toString());
+            postdata.put("content", pesan.getText().toString());
+
+
         } catch (JSONException e) {
 
-            e.printStackTrace();
-        }
+          e.printStackTrace();
+         }
+    }
+    public void APIRegist(View v)  {
+        Intent regist = new Intent(Intent.ACTION_VIEW, Uri.parse("https://mainapi.net/store/client/#/signup"));
+        startActivity(regist);
 
     }
 
@@ -42,32 +63,50 @@ public class MainActivity extends AppCompatActivity {
         task.execute();
     }
 
-    public class GetTask extends AsyncTask<String,String,String> {
+    public class GetTask extends AsyncTask<String, String, JSONObject> {
+        private static final String TAG ="" ;
         private Exception exception;
+        private  Response response;
+        ProgressDialog loading;
+        EditText produk = (EditText) findViewById(R.id.et_produk);
+        protected void onPreExecute(){
+            super.onPreExecute();
+            loading= ProgressDialog.show(MainActivity.this,"Mengambil Data","Loading Data...",false,false);
 
-        protected String doInBackground(String... urls) {
+        }
+
+
+        protected JSONObject doInBackground(String... urls) {
+
             try {
-                String getResponse = get("https://publicobject.com/helloworld.txt");
-                return getResponse;
-            } catch (Exception e) {
-                this.exception = e;
-                return null;
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url("https://sites.google.com/macros/exec?service=AKfycbx_-gZbLP7Z2gGxehXhWMWDAAQsTp3e3bmpTBiaLuzSDQSbIFWD&menu=nama_produk&query="+produk.getText().toString())
+                        .build();
+                response = client.newCall(request).execute();
+                return new JSONObject(response.body().string());
+            } catch (@NonNull IOException | JSONException e) {
+                Log.e(TAG, "" + e.getLocalizedMessage());
             }
+            return null;
         }
 
-        protected void onPostExecute(String getResponse) {
+
+        protected void onPostExecute(JSONObject getResponse) {
+
+            if(loading.isShowing()){
+                loading.dismiss();
+            }
+
+            Toast.makeText(MainActivity.this,getResponse.toString(),Toast.LENGTH_LONG).show();
             System.out.println(getResponse);
+
+
         }
 
-        public String get(String url) throws IOException {
-            Request request = new Request.Builder()
-                    .url(url)
-                    .build();
 
-            Response response = client.newCall(request).execute();
-            return response.body().string();
-        }
     }
+
 
     public void makePostRequest(View v) throws IOException {
         PostTask task = new PostTask();
@@ -76,13 +115,15 @@ public class MainActivity extends AppCompatActivity {
 
     public class PostTask extends AsyncTask<String,String,String> {
         private Exception exception;
-
+        EditText token = (EditText) findViewById(R.id.et_token);
         protected String doInBackground(String... urls) {
             try {
                 String getResponse = post("https://api.mainapi.net/smsnotification/1.0.0/messages",postdata);
+
                 return getResponse;
             } catch (Exception e) {
                 this.exception = e;
+
                 return null;
             }
         }
@@ -97,19 +138,18 @@ public class MainActivity extends AppCompatActivity {
                     .url(url)
                     .addHeader("Content-Type","application/x-www-form-urlencoded")
                     .addHeader("Accept","application/json")
-                    .addHeader("Authorization", "Bearer 1f7363694f1b0ea6a167f3fc531672da")
+                    .addHeader("Authorization",token.getText().toString())
                     .post(body)
                     .build();
             Response response = client.newCall(request).execute();
+
             return response.body().string();
+
         }
     }
-    public String bowlingJson(String msisdn,String content) {
-        content ="hallo";
-        msisdn="082296295744";
 
-        return null;
-    }
+
+
 }
 
 
